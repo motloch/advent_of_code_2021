c996e59421810664682da089a0d61f34abe70759
motloch <18506389+motloch@users.noreply.github.com>
2022-02-02 16:27:28 -0500

    second problem

---
+++ 2.py
@@ -0,0 +1,33 @@
+lines = open('input_2.txt').readlines()
+
+# First part
+horizontal = 0
+depth = 0
+
+for line in lines:
+    t, n = line.replace('\n','').split(' ')
+    if t == 'forward':
+        horizontal += int(n)
+    if t == 'up':
+        depth -= int(n)
+    if t == 'down':
+        depth += int(n)
+        
+print(horizontal*depth)
+    
+# Second part
+horizontal = 0
+depth = 0
+aim = 0
+
+for line in lines:
+    t, n = line.replace('\n','').split(' ')
+    if t == 'forward':
+        horizontal += int(n)
+        depth += aim*int(n)
+    if t == 'up':
+        aim -= int(n)
+    if t == 'down':
+        aim += int(n)
+        
+print(horizontal*depth)
